38e67bdfb2d35eb12232675662bfe9d1f5e69337
Elliott Green <elliottgreen@live.com>
2021-10-14 05:49:56 +0100

    Create wZIL page (#208)
    * Create md page
    
    * add md to sidebar
    
    * add image assets
    
    * Update dev-bridge-resources.md
    
    * Update dev-bridge-resources.md
    
    * Implemented feedback
    
    * Update dev-wrapped-tokens.md
    
    * implement feedback
    
    * feat: update wZIL documentation
    
    Co-authored-by: Jun Hao Tan <junhao@zilliqa.com>

---
+++ docs/dev/dev-wrapped-tokens.md
@@ -0,0 +1,103 @@
+---
+id: dev-wrapped-zil
+title: Wrapped ZIL
+keywords:
+  - wrapped
+  - assets
+  - wzil
+description: wZIL
+---
+
+---
+
+## Introduction to wrap token
+
+A wrapped token is a tokenized version of a cryptocurrency. Wrapped tokens are typically exchangable 1:1 value of the asset they derive from (eg. 1 ZIL = 1 wZIL). Wrapped tokens on Zilliqa implement the ZRC-2 Fungible Token standard. The backing asset is deposited in a smart contract called a "Wrapper" and users are returned a wrapped version of the backing asset.
+
+It's notable to mention that native wrappers are typically designed to be "trustless" and do not need to rely on a third party for the contract to function. Whereas bridging cross-chain, trusted custodians entities take a deposit of a native tokens (ETH) and mint a non-native wrapped version of the asset (zETH) on the destination chain.
+
+## Motivation for wZIL
+
+![Docusaurus](/img/dev/wzil/wZIL.png)
+
+__wZIL__ is a wrapped tokenised version of native ZIL. As mentioned above, anyone may interact with the wZIL contract to either be issued wZIL or be returned native ZIL. This is a trustless contract interaction with no approval being needed by a custodian and the tokens being immediately issued to the caller.  
+
+Developers can take advantage of transitions existing on the ZRC-2 without needing to implement these functions for native ZIL in contract by consuming wZIL.
+
+## Implementation details
+Users call "Mint" with an amount of ZIL (denominated im terms of Qa, the lowest unit for ZIL) and have an equal amount of wZIL minted for the users
+
+```ocaml
+transition Mint()
+  accept; (* Contract accepts all deposits made to it *)
+  AuthorizedMint _sender _amount;    (* Mints _amount of wZIL to _sender *)
+  ...
+end
+```
+
+To get back native ZIL, users can call the  "Burn" transition. This will 
+
+1. Transfer a certain amount of wZIL back to the wZIL contract. 
+2. wZIL contract burn away the wZIL received
+3. wZIL contract transfer native ZIL to the users
+
+
+```ocaml
+transition Burn(amount: Uint128)
+  AuthorizedBurnIfSufficientBalance _sender amount;    (* Burns amount *)
+  msg_to_sender = {_tag : "AddFunds"; _recipient : _sender; _amount : amount};    (* Returns _amount of ZIL to _sender *)
+  ...
+end
+```
+
+wZIL can be consumed from the following addresses.
+
+|               | Contract Address                                                                                                                                |
+| ------------- | ----------------------------------------------------------------------------------------------------------------------------------------------- |
+| Mainnet wZIL  | [`zil1gvr0jgwfsfmxsyx0xsnhtlte4gks6r3yk8x5fn`](https://viewblock.io/zilliqa/address/zil1gvr0jgwfsfmxsyx0xsnhtlte4gks6r3yk8x5fn)                 |
+| Testnet wZIL  | [`zil1nzn3k336xwal7egdzgalqnclxtgu3dggxed85m`](https://viewblock.io/zilliqa/address/zil1nzn3k336xwal7egdzgalqnclxtgu3dggxed85m?network=testnet) |
+
+
+## How to wrap/unwrap wZIL
+
+Two examples are shown below, the first being how to wrap and unwrap wZIL using Pillar Protocols user interface. The second example shows how to directly call the contract using the Neo Savant IDE. 
+
+
+### Interacting with Pillar Protocol
+
+ [`Pillar Protocol`](https://app.pillarprotocol.com/vaultFactory/WZIL) has a graphical interface which can be used to wrap ZIL or unwrap wZIL. 
+Users can interact directly with the wZIL contract from the web interface once they are signed into their wallet.
+
+<b> When logged into the Pillar Protocols wZIL vault, buttons are displayed for converting ZIL/wZIL. </b>
+
+![Docusaurus](/img/dev/wzil/pillar_wzil.png)
+
+
+### Interacting with Neo-Savant IDE
+
+Users may choose to manually wrap or unwrap ZIL manually from the contract. To wrap or unwrap tokens, firstly import the wZIL contract to the Neo-Savant IDE.
+
+<b> Open the import contract window and import the wZIL contract. </b>
+
+![Docusaurus](/img/dev/wzil/import_contract_1.png)
+
+<b> Call Mint with the QA amount of ZIL required to be wrapped. </b>
+
+![Docusaurus](/img/dev/wzil/mint_wzil_1.png)
+
+<b> Successful mint of wZIL in exchange for ZIL. </b>
+
+![Docusaurus](/img/dev/wzil/mint_wzil_2.png)
+
+<b> Having a wZIL token, call burn with the amount as a parameter.</b>
+
+![Docusaurus](/img/dev/wzil/burn_wzil_1.png)
+
+<b> Note the internal transaction returning ZIL from the wrapper in exchange for an equal burn of wZIL.</b>
+
+![Docusaurus](/img/dev/wzil/burn_wzil_2.png)
+
+
+### Interacting programmatically
+
+You can refer to https://github.com/Zilliqa/zilwrap-sdk for more information on how to use the SDK for wZIL.
--- sidebars.js
+++ sidebars.js
@@ -1,150 +1,163 @@
 module.exports = {
   BasicsSideBar: {
-    "Blockchain Basics": ["basics/basics-intro-blockchain","basics/basics-intro-consensus","basics/basics-intro-accounts",
-    "basics/basics-intro-txns", "basics/basics-intro-gas"],
+    "Blockchain Basics": ["basics/basics-intro-blockchain", "basics/basics-intro-consensus", "basics/basics-intro-accounts",
+      "basics/basics-intro-txns", "basics/basics-intro-gas"],
 
-    "Zilliqa Architecture": ["basics/basics-zil-nodes","basics/basics-zil-sharding","basics/basics-zil-consensus",
-    "basics/basics-zil-schnorr-signatures", "basics/basics-zil-reward","basics/basics-zil-contract","basics/basics-zil-gas"],
+    "Zilliqa Architecture": ["basics/basics-zil-nodes", "basics/basics-zil-sharding", "basics/basics-zil-consensus",
+      "basics/basics-zil-schnorr-signatures", "basics/basics-zil-reward", "basics/basics-zil-contract", "basics/basics-zil-gas"],
 
     "Zilliqa Ecosystem": ["basics/overview"]
   },
   APIsSideBar: {
     "Introduction": ["apis/api-introduction"],
-    "Blockchain-related Methods":["apis/api-blockchain-ds-block-listing",
-    "apis/api-blockchain-get-blockchain-info",
-    "apis/api-blockchain-get-current-ds-epoch",
-    "apis/api-blockchain-get-current-mini-epoch",
-    "apis/api-blockchain-get-ds-block",
-    "apis/api-blockchain-get-ds-block-rate",
-    "apis/api-blockchain-get-latest-ds-block",
-    "apis/api-blockchain-get-latest-tx-block",
-    "apis/api-blockchain-get-miner-info",
-    "apis/api-blockchain-get-network-id",
-    "apis/api-blockchain-num-ds-blocks",
-    "apis/api-blockchain-get-num-tx",
-    "apis/api-blockchain-get-num-tx-blocks",
-    "apis/api-blockchain-get-prev-difficulty",
-    "apis/api-blockchain-get-prev-ds-difficulty",
-    "apis/api-blockchain-get-total-coin-supply",
-    "apis/api-blockchain-get-tx-rate",
-    "apis/api-blockchain-get-tx-block",
-    "apis/api-blockchain-get-tx-block-rate",
-    "apis/api-blockchain-tx-block-listing"],
+    "Blockchain-related Methods": ["apis/api-blockchain-ds-block-listing",
+      "apis/api-blockchain-get-blockchain-info",
+      "apis/api-blockchain-get-current-ds-epoch",
+      "apis/api-blockchain-get-current-mini-epoch",
+      "apis/api-blockchain-get-ds-block",
+      "apis/api-blockchain-get-ds-block-rate",
+      "apis/api-blockchain-get-latest-ds-block",
+      "apis/api-blockchain-get-latest-tx-block",
+      "apis/api-blockchain-get-miner-info",
+      "apis/api-blockchain-get-network-id",
+      "apis/api-blockchain-num-ds-blocks",
+      "apis/api-blockchain-get-num-tx",
+      "apis/api-blockchain-get-num-tx-blocks",
+      "apis/api-blockchain-get-prev-difficulty",
+      "apis/api-blockchain-get-prev-ds-difficulty",
+      "apis/api-blockchain-get-total-coin-supply",
+      "apis/api-blockchain-get-tx-rate",
+      "apis/api-blockchain-get-tx-block",
+      "apis/api-blockchain-get-tx-block-rate",
+      "apis/api-blockchain-tx-block-listing"],
 
-    "Transaction-related Methods":["apis/api-transaction-create-tx",
-    "apis/api-transaction-get-minimum-gas-price",
-    "apis/api-transaction-get-num-txns-dsepoch",
-    "apis/api-transaction-get-num-txns-txepoch",
-    "apis/api-transaction-get-recent-txs",
-    "apis/api-transaction-get-tx",
-    "apis/api-transaction-get-transaction-status",
-    "apis/api-transaction-get-txs-for-txblock",
-    "apis/api-transaction-get-txs-for-txblock-ex",
-    "apis/api-transaction-get-txbodies-for-txblock",
-    "apis/api-transaction-get-txbodies-for-txblock-ex"],
+    "Transaction-related Methods": ["apis/api-transaction-create-tx",
+      "apis/api-transaction-get-minimum-gas-price",
+      "apis/api-transaction-get-num-txns-dsepoch",
+      "apis/api-transaction-get-num-txns-txepoch",
+      "apis/api-transaction-get-recent-txs",
+      "apis/api-transaction-get-tx",
+      "apis/api-transaction-get-transaction-status",
+      "apis/api-transaction-get-txs-for-txblock",
+      "apis/api-transaction-get-txs-for-txblock-ex",
+      "apis/api-transaction-get-txbodies-for-txblock",
+      "apis/api-transaction-get-txbodies-for-txblock-ex"],
 
-    "Contract-related Methods":["apis/api-contract-get-contractaddress-from-txid",
-    "apis/api-contract-get-smartcontract-code",
-    "apis/api-contract-get-smartcontract-init",
-    "apis/api-contract-get-smartcontracts",
-    "apis/api-contract-get-smartcontract-state",
-    "apis/api-contract-get-smartcontract-substate",
-    "apis/api-contract-get-state-proof"],
+    "Contract-related Methods": ["apis/api-contract-get-contractaddress-from-txid",
+      "apis/api-contract-get-smartcontract-code",
+      "apis/api-contract-get-smartcontract-init",
+      "apis/api-contract-get-smartcontracts",
+      "apis/api-contract-get-smartcontract-state",
+      "apis/api-contract-get-smartcontract-substate",
+      "apis/api-contract-get-state-proof"],
 
-    "Account-related Methods":["apis/api-account-get-balance"]
+    "Account-related Methods": ["apis/api-account-get-balance"]
   },
   DevelopersSidebar: {
 
-    "Getting Started": ["dev/dev-started-introduction","dev/dev-started-helloworld","dev/dev-started-env"],
+    "Getting Started": ["dev/dev-started-introduction", "dev/dev-started-helloworld", "dev/dev-started-env"],
     "Upgrade Notices": ["dev/dev-upgrade-v8"],
 
-    "User Key Management": ["dev/dev-keys-introduction","dev/dev-keys-zilpay","dev/dev-keys-pkey", "dev/dev-keys-zrc2-wallet-support", "dev/dev-keys-magic"],
+    "User Key Management": ["dev/dev-keys-introduction", "dev/dev-keys-zilpay", "dev/dev-keys-pkey", "dev/dev-keys-zrc2-wallet-support", "dev/dev-keys-magic"],
 
-    "Developer Toolings": ["dev/dev-tools-ceres", {"SDKs":["dev/dev-tools-zilliqajs","dev/dev-tools-gozilliqa","dev/dev-tools-java", "dev/dev-tools-sdks"]},
-    "dev/dev-tools-websockets", "dev/dev-tools-cli","dev/dev-tools-ide","dev/dev-tools-explorer","dev/dev-tools-faucet"],
+    "Developer Toolings": ["dev/dev-tools-ceres", { "SDKs": ["dev/dev-tools-zilliqajs", "dev/dev-tools-gozilliqa", "dev/dev-tools-java", "dev/dev-tools-sdks"] },
+      "dev/dev-tools-websockets", "dev/dev-tools-cli", "dev/dev-tools-ide", "dev/dev-tools-explorer", "dev/dev-tools-faucet"],
 
-    "Transaction Lifecycle": ["dev/dev-txn-signing","dev/dev-txn-broadcasting","dev/dev-txn-polling","dev/dev-txn-confirmation",
-    "dev/dev-txn-receipt"],
+    "Other Developer Information": ["dev/dev-wrapped-zil"],
+
+    "Transaction Lifecycle": ["dev/dev-txn-signing", "dev/dev-txn-broadcasting", "dev/dev-txn-polling", "dev/dev-txn-confirmation",
+      "dev/dev-txn-receipt"],
+
     "Educational Resources": [
       "dev/dev-education-videos",
-      {"Sample app – RentOnZilliqa":["dev/dev-rentonzilliqa-introduction",{"Scilla Contract":["dev/dev-rentonzilliqa-contract","dev/dev-rentonzilliqa-library","dev/dev-rentonzilliqa-mutable-variables","dev/dev-rentonzilliqa-procedures","dev/dev-rentonzilliqa-transitions"]},{"Frontend Application":["dev/dev-rentonzilliqa-frontend","dev/dev-rentonzilliqa-components", "dev/dev-rentonzilliqa-scripting", "dev/dev-rentonzilliqa-modals", "dev/dev-rentonzilliqa-pages"]}]},
+      { "Sample app – RentOnZilliqa": ["dev/dev-rentonzilliqa-introduction", { "Scilla Contract": ["dev/dev-rentonzilliqa-contract", "dev/dev-rentonzilliqa-library", "dev/dev-rentonzilliqa-mutable-variables", "dev/dev-rentonzilliqa-procedures", "dev/dev-rentonzilliqa-transitions"] }, { "Frontend Application": ["dev/dev-rentonzilliqa-frontend", "dev/dev-rentonzilliqa-components", "dev/dev-rentonzilliqa-scripting", "dev/dev-rentonzilliqa-modals", "dev/dev-rentonzilliqa-pages"] }] },
     ]
   },
   MinersSidebar: {
-    "Miners":["miners/mining-getting-started","miners/mining-zilclient","miners/mining-zilminer","miners/mining-proxy",
-    "miners/mining-additional-info"]
+    "Miners": ["miners/mining-getting-started", "miners/mining-zilclient", "miners/mining-zilminer", "miners/mining-proxy",
+      "miners/mining-additional-info"]
   },
   ExchangesSidebar: {
-    "Exchange Integration":[
-      {"Getting Started" : ["exchanges/exchange-getting-started", "exchanges/exchange-ip-whitelisting", "exchanges/exchange-key-whitelisting-1", "exchanges/exchange-key-whitelisting-2"]},
+    "Exchange Integration": [
+      { "Getting Started": ["exchanges/exchange-getting-started", "exchanges/exchange-ip-whitelisting", "exchanges/exchange-key-whitelisting-1", "exchanges/exchange-key-whitelisting-2"] },
       "exchanges/exchange-account-management",
       "exchanges/exchange-sending-transactions",
       "exchanges/exchange-tracking-deposits",
       "exchanges/exchange-transaction-receipts",
       "exchanges/exchange-managing-zrc2-tokens"],
     "Rosetta": [
-      {"Introduction": ["exchanges/rosetta-introduction", "exchanges/rosetta-unsupported-api", "exchanges/rosetta-setting-up-seed-node", "exchanges/rosetta-setting-up-no-seed-node"]},
-      {"Data API" : [{
-        "Network" : [
-          "exchanges/rosetta-data-network-list",
-          "exchanges/rosetta-data-network-options",
-          "exchanges/rosetta-data-network-status"],
-        "Account" : ["exchanges/rosetta-data-account-balance"], 
-        "Block" : [
-          "exchanges/rosetta-data-block",
-          "exchanges/rosetta-data-block-transaction"],
-        "Mempool": ["exchanges/rosetta-data-mempool"] 
-        }]},
-      {"Construction API" : [
-        "exchanges/rosetta-construction",
-        "exchanges/rosetta-construction-derive",
-        "exchanges/rosetta-construction-preprocess", 
-        "exchanges/rosetta-construction-metadata",
-        "exchanges/rosetta-construction-payloads",
-        "exchanges/rosetta-construction-parse", 
-        "exchanges/rosetta-construction-combine",
-        "exchanges/rosetta-construction-hash",
-        "exchanges/rosetta-construction-submit",
-      ]} 
+      { "Introduction": ["exchanges/rosetta-introduction", "exchanges/rosetta-unsupported-api", "exchanges/rosetta-setting-up-seed-node", "exchanges/rosetta-setting-up-no-seed-node"] },
+      {
+        "Data API": [{
+          "Network": [
+            "exchanges/rosetta-data-network-list",
+            "exchanges/rosetta-data-network-options",
+            "exchanges/rosetta-data-network-status"],
+          "Account": ["exchanges/rosetta-data-account-balance"],
+          "Block": [
+            "exchanges/rosetta-data-block",
+            "exchanges/rosetta-data-block-transaction"],
+          "Mempool": ["exchanges/rosetta-data-mempool"]
+        }]
+      },
+      {
+        "Construction API": [
+          "exchanges/rosetta-construction",
+          "exchanges/rosetta-construction-derive",
+          "exchanges/rosetta-construction-preprocess",
+          "exchanges/rosetta-construction-metadata",
+          "exchanges/rosetta-construction-payloads",
+          "exchanges/rosetta-construction-parse",
+          "exchanges/rosetta-construction-combine",
+          "exchanges/rosetta-construction-hash",
+          "exchanges/rosetta-construction-submit",
+        ]
+      }
     ]
   },
   StakingSidebar: {
-    "Zilliqa Seed Node Staking":["staking/staking-overview", "staking/staking-disclaimer"],
-    "Staking Phase 1.1":[
-    "staking/phase1/staking-phase11-notice",
-    "staking/phase1/staking-phase1-overview",
-    "staking/phase1/staking-general-information",
-    {"Delegators":
-    ["staking/phase1/delegator/staking-delegator-overview",
-    "staking/phase1/delegator/staking-delegator-reading-contract-states", 
-    "staking/phase1/delegator/staking-delegator-operations",
-    "staking/phase1/delegator/staking-delegator-gzil"]},
-    {"SSN Operators":[
-        "staking/phase1/ssn-operator/staking-ssn-before-you-start",
-        "staking/phase1/ssn-operator/staking-ssn-setup",
-        "staking/phase1/ssn-operator/staking-ssn-enrollment",
-        "staking/phase1/ssn-operator/staking-commission-management",
-        "staking/phase1/ssn-operator/staking-ssn-maintenance",
-        "staking/phase1/ssn-operator/staking-ssn-upgrading",]},
+    "Zilliqa Seed Node Staking": ["staking/staking-overview", "staking/staking-disclaimer"],
+    "Staking Phase 1.1": [
+      "staking/phase1/staking-phase11-notice",
+      "staking/phase1/staking-phase1-overview",
+      "staking/phase1/staking-general-information",
+      {
+        "Delegators":
+          ["staking/phase1/delegator/staking-delegator-overview",
+            "staking/phase1/delegator/staking-delegator-reading-contract-states",
+            "staking/phase1/delegator/staking-delegator-operations",
+            "staking/phase1/delegator/staking-delegator-gzil"]
+      },
+      {
+        "SSN Operators": [
+          "staking/phase1/ssn-operator/staking-ssn-before-you-start",
+          "staking/phase1/ssn-operator/staking-ssn-setup",
+          "staking/phase1/ssn-operator/staking-ssn-enrollment",
+          "staking/phase1/ssn-operator/staking-commission-management",
+          "staking/phase1/ssn-operator/staking-ssn-maintenance",
+          "staking/phase1/ssn-operator/staking-ssn-upgrading",]
+      },
 
-        "staking/phase1/staking-error-codes",
+      "staking/phase1/staking-error-codes",
     ],
   },
   ContributorsSidebar: {
-    "Contributors":["contributors/contribute-buildzil","contributors/contribute-guidelines","contributors/contribute-standards",
-    "contributors/contribute-bug-bounty"],
+    "Contributors": ["contributors/contribute-buildzil", "contributors/contribute-guidelines", "contributors/contribute-standards",
+      "contributors/contribute-bug-bounty"],
 
-    "Core Protocol Design":[
-    {"Design Overview":["contributors/core-node-operation"]}, 
-    {"Consensus Layer":["contributors/core-consensus","contributors/core-multisignatures"]},
-    {"Network Layer":["contributors/core-gossip","contributors/core-broadcasting","contributors/core-blacklist","contributors/core-messaging-limits"]},
-    {"Messaging Layer":["contributors/core-message-dispatch","contributors/core-message-queues"]},
-    {"Data Layer": ["contributors/core-incremental-db"]},
-    {"Directory Service": ["contributors/core-ds-mimo","contributors/core-ds-reputation"]},
-    {"Lookup": ["contributors/core-websocket-server","contributors/core-transaction-dispatch","contributors/core-multipliers"]},
-    {"Mining": ["contributors/core-pow","contributors/core-difficulty-adjustment","contributors/core-por","contributors/core-coinbase","contributors/core-global-gas-price"]},
-    {"Mitigation Measures": ["contributors/core-guard-mode","contributors/core-rejoin-mechanism","contributors/core-view-change",
-    "contributors/core-diagnostic-data","contributors/core-status-server"]}
-  ]
+    "Core Protocol Design": [
+      { "Design Overview": ["contributors/core-node-operation"] },
+      { "Consensus Layer": ["contributors/core-consensus", "contributors/core-multisignatures"] },
+      { "Network Layer": ["contributors/core-gossip", "contributors/core-broadcasting", "contributors/core-blacklist", "contributors/core-messaging-limits"] },
+      { "Messaging Layer": ["contributors/core-message-dispatch", "contributors/core-message-queues"] },
+      { "Data Layer": ["contributors/core-incremental-db"] },
+      { "Directory Service": ["contributors/core-ds-mimo", "contributors/core-ds-reputation"] },
+      { "Lookup": ["contributors/core-websocket-server", "contributors/core-transaction-dispatch", "contributors/core-multipliers"] },
+      { "Mining": ["contributors/core-pow", "contributors/core-difficulty-adjustment", "contributors/core-por", "contributors/core-coinbase", "contributors/core-global-gas-price"] },
+      {
+        "Mitigation Measures": ["contributors/core-guard-mode", "contributors/core-rejoin-mechanism", "contributors/core-view-change",
+          "contributors/core-diagnostic-data", "contributors/core-status-server"]
+      }
+    ]
   },
 };
